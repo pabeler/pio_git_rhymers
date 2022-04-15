a57088f5b1f95dba4441a79c8654abf444b08501
pabeler <pabeler@o2.pl>
2022-04-15 19:45:30 +0200

    2. Utworzenie gałęzi format oraz zformatowanie kodu źródłowego we wszystkich klasach

--- src/main/java/edu/kis/vh/nursery/HanoiRhymer.java
+++ src/main/java/edu/kis/vh/nursery/HanoiRhymer.java
@@ -2,16 +2,19 @@ package edu.kis.vh.nursery;
 
 public class HanoiRhymer extends defaultCountingOutRhymer {
 
-int totalRejected = 0;
+    int totalRejected = 0;
 
-	public int reportRejected() {
-		return totalRejected;
-	}
+    public int reportRejected() {
+        return totalRejected;
+    }
 
-	public void countIn(int in) {
-	if (!callCheck() && in > peekaboo())
-			totalRejected++;
-			else
-				super.countIn(in);
+    public void countIn(int in) {
+        if (!callCheck() && in > peekaboo())
+            totalRejected++;
+        else
+            super.countIn(in);
 	}
 }
+
+//W klasie HanoiRhymer źle sformatowane byy wiersze: 5, 12, 14, 15
+//Kombinacje klawiszy alt + ->, alt + <- umożliwiają przełączanie między otwartymi oknami w ide
